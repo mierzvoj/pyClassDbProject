03c7138aecac129a53ed995eb7a16f6cf0bb238a
mierzvoj <mierzvoj@gmail.com>
2022-04-03 21:16:28 +0200

    messing with DB

--- database/users_model.py
+++ database/users_model.py
@@ -1,4 +1,38 @@
+import sqlite3
+from os import getenv
+from sys import argv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+engine = create_engine('sqlite:///sqlalchemy.sqlite', echo=True)
+base = declarative_base()
+
+
+class User(base):
+    __tablename__ = 'users'
+
+    users_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    password = Column(String)
+
+    def __init__(self, users_id, name, password):
+        self.users_id = users_id
+        self.name = name
+        self.password = password
+
+
+base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from os import getenv
+
+from sqlalchemy.orm import sessionmaker
+
+from database import users_model
+from database.users_model import User
 from users.users_service import LoggingApi
+from database.database import Database
 import sqlite3
 from sqlite3 import Error
 
 
-
-
-
 def run():
     if __name__ == '__main__': run()
-
-
 
 
 def begin():
@@ -36,8 +38,27 @@ def createNewLoggingApi():
     return newapi
 
 
-import os
+def createNewDatabase():
+    newdatabase = User(getenv('DB_NAME'))
+    return newdatabase
 
-print(os.getcwd())
+
+print(getenv("DB_NAME"))
+
+insert1 = Database()
+insert1.insert()
+
+Session = sessionmaker(bind=users_model.engine)
+session = Session()
+
+us = users_model.User(1, 'wojtek3', 'login1234')
+session.add(us)
+
+session.commit()
+
+
+
+u = User('1000', 'wojtek2', 'blabla')
+insert1.add(u)
 
 begin()
