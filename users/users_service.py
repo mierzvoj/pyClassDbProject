2e411529d817796957dfdb7a4655498ffd8f4ef3
mierzvoj <mierzvoj@gmail.com>
2022-04-05 21:14:08 +0200

    login, new user register, login verifying finally works

--- users/users_service.py
+++ users/users_service.py
@@ -8,29 +8,19 @@ import getpass
 import bcrypt
 
 
-
-
 class UserManager(SessionManager):
     Base = declarative_base()
     engine = create_engine('sqlite:///sqlalchemy.sqlite', echo=True)
 
-
     def __init__(self):
         self.name = None
         self.password = None
-        self.islogged = None
+        self.islogged = False
         self.hashed = None
         self.path = None
         self.new_user = None
-
-    # def insert(self):
-    #     print("insert only")
-    #     Session = sessionmaker(bind=UserManager.engine)
-    #     session = Session()
-    #     new_user = users_model.User("john12", "blabla")
-    #     session.add(new_user)
-    #     session.commit()
-
+        self.bytespass = None
+        self.user_passwd = None
 
     def createNewUser(self):
         print("tu createNewUser")
@@ -60,44 +50,44 @@ class UserManager(SessionManager):
         print("commit unsuccesfull")
 
     def login(self):
-        self.islogged = False
-
         while not self.islogged:
-            userdata = []
-            with open('users.csv', 'r') as csvfile:
-                reader = csv.reader(csvfile)
-                for row in reader:
-                    userdata.append(row)
             self.name = input('Podaj swój login: ')
-            self.password = getpass.getpass('Podaj swoje hasło ')
-            col0 = [x[0] for x in userdata]
-            col1 = [x[1] for x in userdata]
-            if self.name in col0:
-                for k in range(0, len(col0)):
-                    if col0[k] == self.name and col1[k] == bcrypt.checkpw(self.password, self.hashed):
-                        print("Zalogowałeś się ")
-                        self.islogged = True
+            self.password = getpass.getpass("Podaj swoje hasło: ")
+            self.bytespass = b"self.password"
+            self.hashed = bcrypt.hashpw(self.bytespass, bcrypt.gensalt())
+            Session = sessionmaker(bind=UserManager.engine)
+            session = Session()
+            user = session.query(session.query(User).filter_by(name=self.name).exists()).scalar()
+            user_passwd = session.query(User.password).filter(User.name == self.name).one_or_none()
+            result = user_passwd[0]
+            print(user)
+            print(result)
+            print(self.hashed)
+            if user and bcrypt.hashpw(self.bytespass, result):
+                print("Zalogowałeś się ")
+                self.islogged = True
+                # session['logged_in'] = True
             else:
                 print('Nieprawidłowy login lub hasło')
                 print('Spróbuj się zarejestrować')
-                self.register()
+                self.createNewUser()
+        print("tu options")
         self.options()
 
     def verifyLogin(self, name):
-        csv_file = csv.reader(open("users.csv", "r"))
         if not any(char.isdigit() for char in name):
             print('Podaj choć jedną cyfrę w loginie użytkownika')
-            self.register()
+            self.createNewUser()
         else:
             while True:
-                for row in csv_file:
-                    if name == row[0]:
+                Session = sessionmaker(bind=UserManager.engine)
+                session = Session()
+                user = session.query(session.query(User).filter_by(name=self.name).exists()).scalar()
+                if user:
                         print("Użytkownik o takim loginie już istnieje")
                         print("Zarejestruj się jako nowy użytkownik o innym loginie")
-                        self.register()
+                        self.createNewUser()
                 break
-
-
 
     def options(self):
         while self.islogged:
@@ -105,13 +95,10 @@ class UserManager(SessionManager):
             print("Wybierz opcję menu lista: 1/szukaj: 2/usun: 3/zakoncz: 4\n")
             menu = int(input("podaj wybór: "))
             if menu == 1:
-                self.displayUsers()
+                print("display all users")
             elif menu == 2:
-                self.searchByLogin()
+                print("search")
             elif menu == 3:
-                self.deleteEntry()
                 print("Wybrany użytkownik został usunięty z rejestru, tej operacji nie można odwrócić")
             else:
                 exit()
-
-
